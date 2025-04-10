bbf086e47d9c64823137f3f5cfee07977a4e8814
Sam Vente <savente93@proton.me>
2025-04-10 21:15:54 +0200

    feat(tests): add integration test setup

--- tests/main.rs
+++ tests/main.rs
@@ -1,8 +1,17 @@
 
-use anyhow::Result;
+#![allow(unused_imports)]
+use pretty_assertions::{assert_eq, assert_ne};
 
 use {{crate_name}}::*;
+use color_eyre::eyre::Result;
+
 
 pub fn setup() -> Result<()> {
     Ok(())
+}
+
+
+#[test]
+fn test_example() {
+    // integration tests go here
 }
