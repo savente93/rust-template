a88f4e9c300a985d1e47af7fafa61f8c1b5a7cf1
Sam Vente <savente93@proton.me>
2025-04-15 18:21:17 +0200

    feat(template): custom help messages for verbosity

--- src/cli.rs
+++ src/cli.rs
@@ -1,8 +1,29 @@
 use clap::Parser;
+use clap_verbosity_flag::{LogLevel, Verbosity, VerbosityFilter};
 
-#[derive(Parser, Debug)]
+pub struct CustomLogLevel {}
+
+impl LogLevel for CustomLogLevel {
+    fn default_filter() -> VerbosityFilter {
+        VerbosityFilter::Error
+    }
+    fn quiet_help() -> Option<&'static str> {
+        Some("suppress all logging output")
+    }
+    fn quiet_long_help() -> Option<&'static str> {
+        Some("Surpress the logging output of the application, including errors.")
+    }
+    fn verbose_help() -> Option<&'static str> {
+        Some("Increase verbosity of the logging (can be specified multiple times).")
+    }
+    fn verbose_long_help() -> Option<&'static str> {
+        Some("Increase the logging verbosity of the application by one level (ERROR, WARN, INFO, DEBUG, TRACE)")
+    }
+}
+
+#[derive(Parser)]
 #[command(version, about, long_about= None)]
 pub struct Args {
     #[command(flatten)]
-    verbose: clap_verbosity_flag::Verbosity,
+    pub verbose: Verbosity,
 }
